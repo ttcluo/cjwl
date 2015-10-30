feaffd6d203f3675796ace2f6436ec6b79a14622
larno <loch1992@icloud.com>
2015-10-29 23:16:59 -0700

    service_fragment界面实现

--- app/src/main/java/com/htlc/cjwl/fragment/ServiceFragment.java
+++ app/src/main/java/com/htlc/cjwl/fragment/ServiceFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -14,20 +15,22 @@ import com.htlc.cjwl.R;
 import com.htlc.cjwl.adapter.ServiceListViewAdapter;
 import com.htlc.cjwl.bean.ServiceBean;
 import com.htlc.cjwl.bean.ServiceItemBean;
+import com.htlc.cjwl.util.LogUtil;
 
 import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2015/10/28.
  */
-public class ServiceFragment extends Fragment{
+public class ServiceFragment extends Fragment implements AdapterView.OnItemClickListener {
     private ListView lv_service_listView;
     private ServiceListViewAdapter adapter;
+    private ArrayList<ServiceItemBean> items;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.layout_home_fragment,null);
+        View view = inflater.inflate(R.layout.layout_service_fragment,null);
 
         TextView tv_fragment_title = (TextView) view.findViewById(R.id.tv_fragment_title);
         tv_fragment_title.setText(R.string.service_fragment_title);
@@ -41,7 +44,7 @@ public class ServiceFragment extends Fragment{
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         //测试数据------------------
-       ArrayList<ServiceItemBean> items = new ArrayList<ServiceItemBean>();
+        items = new ArrayList<ServiceItemBean>();
         for(int i=0;i<10;i++){
             ServiceItemBean bean = new ServiceItemBean();
             bean.id = i;
@@ -50,9 +53,17 @@ public class ServiceFragment extends Fragment{
         }
 
         //测试数据------------------
-//        System.out.println(getContext());
+
         adapter = new ServiceListViewAdapter(getContext(), items);
-        System.out.println(adapter);
         lv_service_listView.setAdapter(adapter);
+        lv_service_listView.setOnItemClickListener(this);
+
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        ServiceItemBean serviceItemBean = items.get(position);
+        LogUtil.i(this,serviceItemBean.id+"");
     }
 }
