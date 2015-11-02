a1ce9e778ddb5cf543ec900733e237abbea25aac
luochuan <loch1992@icloud.com>
2015-11-02 17:07:59 +0800

    订单界面实现优化，实现下拉刷新

--- app/src/main/java/com/htlc/cjwl/fragment/OrdersFragment.java
+++ app/src/main/java/com/htlc/cjwl/fragment/OrdersFragment.java
@@ -14,33 +14,40 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.astuetz.PagerSlidingTabStrip;
 import com.htlc.cjwl.R;
 import com.htlc.cjwl.adapter.OrdersPagerAdaptor;
 import com.htlc.cjwl.util.CommonUtil;
-import com.htlc.cjwl.widget.PagerSlidingTab;
-import butterknife.ButterKnife;
+
 import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by Administrator on 2015/10/28.
  */
 public class OrdersFragment extends Fragment implements ActionBar.TabListener {
 
-    private PagerSlidingTab indicator;
-    private ViewPager viewPager;
+    @Bind(R.id.indicator)
+    PagerSlidingTabStrip indicator;
+    @Bind(R.id.viewPager)
+    ViewPager viewPager;
+    private int id = 0;
+
     private LinearLayout mTabsLinearLayout;
 
-    public static OrdersFragment newInstance() {
+    public static OrdersFragment newInstance(int id) {
         OrdersFragment ordersFragment = new OrdersFragment();
+        ordersFragment.id = id;
         return ordersFragment;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_orders_fragment, null);
-        indicator = (PagerSlidingTab) view.findViewById(R.id.indicator);
+        ButterKnife.bind(this, view);
         TextView tv_fragment_title = (TextView) view.findViewById(R.id.tv_fragment_title);
-        tv_fragment_title.setText("我的订单");
-        viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        // tv_fragment_title.setText("我的订单");
+
         Init();
 
         return view;
@@ -50,40 +57,72 @@ public class OrdersFragment extends Fragment implements ActionBar.TabListener {
         InitTab();
         viewPager.setAdapter(new OrdersPagerAdaptor(getChildFragmentManager()));
         indicator.setViewPager(viewPager);
+        indicator.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                indicator.setTranslationX(0);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < mTabsLinearLayout.getChildCount(); i++) {
+                    TextView tv = (TextView) mTabsLinearLayout.getChildAt(i);
+                    if (i == position) {
+
+                        tv.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Medium.ttf"));
+                    } else {
+                        tv.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Light.ttf"));
+
+                    }
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
         mTabsLinearLayout = (LinearLayout) indicator.getChildAt(0);
-//        for (int i = 0; i < mTabsLinearLayout.getChildCount(); i++) {
-//            TextView tv = (TextView) mTabsLinearLayout.getChildAt(i);
-//            if (i == 0) {
-//
-//                tv.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Medium.ttf"));
-//            } else {
-//                tv.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Light.ttf"));
-//
-//            }
-//        }
+        for (int i = 0; i < mTabsLinearLayout.getChildCount(); i++) {
+            TextView tv = (TextView) mTabsLinearLayout.getChildAt(i);
+            if (i == 0) {
+
+                tv.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Medium.ttf"));
+            } else {
+                tv.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Light.ttf"));
+
+            }
+        }
     }
 
     public void InitTab() {
-        //tabtitle字体大写
-        indicator.setAllCaps(true);
-        //tab 宽度均分
-        indicator.setShouldExpand(true);
-        //indicator.setTextSize(CommonUtil.dp2px(this.getActivity(), 18));
-        //设置下划线
-        indicator.setUnderlineColor(Color.BLACK);
-        indicator.setUnderlineHeight(CommonUtil.dp2px(this.getActivity(), 1));
+
+         //tab 宽度均分
+         indicator.setShouldExpand(true);
+         indicator.setTextSize(CommonUtil.dp2px(this.getActivity(), 14));
+        indicator.setTextColor(R.color.order);
+         //设置下划线
+//        indicator.setUnderlineColor(Color.BLACK);
+//        indicator.setUnderlineHeight(CommonUtil.dp2px(this.getActivity(), 1));
         //设置滑动指示线
-        indicator.setIndicatorColor(R.color.red);
-        indicator.setIndicatorHeight(CommonUtil.dp2px(this.getActivity(), 4));
-        //设置tab间分割线
-        indicator.setDividerColor(Color.TRANSPARENT);
+          indicator.setIndicatorColor(R.color.order);
+          indicator.setIndicatorHeight(CommonUtil.dp2px(this.getActivity(), 2));
+//        //设置tab间分割线
+//        indicator.setDividerColor(Color.TRANSPARENT);
         //设置背景颜色
         indicator.setBackgroundColor(this.getResources().getColor(R.color.white));
     }
 
     @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
+    @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
 
+        viewPager.setCurrentItem(tab.getPosition());
     }
 
     @Override
